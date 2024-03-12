01b4f075f6a3371bcccd1f0c69b01ed54dbc2881
slayeh17 <debakroy7@gmail.com>
2024-03-12 17:45:29 +0530

    update settings.py

--- backend/Tinker/Tinker/settings.py
+++ backend/Tinker/Tinker/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# from dotenv import load_dotenv
-# dotenv_path = BASE_DIR / ".env"
-# load_dotenv(dotenv_path)
+from dotenv import load_dotenv
+dotenv_path = BASE_DIR / ".env"
+load_dotenv(dotenv_path)
 
-SECRET_KEY = ""
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
