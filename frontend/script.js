0b1c80507a8fa788240a4142f4f14ce313d3d8cf
AKASH BOSE <akashbose2002@gmail.com>
2024-03-12 10:10:48 +0530

    error crrct init

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("active");
+});
