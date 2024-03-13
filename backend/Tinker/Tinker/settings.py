aa9a93ca23a6075a7e5978ca36c46164e0e36484
slayeh17 <debakroy7@gmail.com>
2024-03-13 23:58:42 +0530

    update settings.py

--- backend/Tinker/Tinker/settings.py
+++ backend/Tinker/Tinker/settings.py
@@ -141,3 +141,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     'http://tinkerquest.onrender.com',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
