4fbe322020c621241a76100b3b21c84b5ce6ba11
slayeh17 <debakroy7@gmail.com>
2024-03-13 20:45:50 +0530

    add postgres db from railway

--- backend/Tinker/Tinker/settings.py
+++ backend/Tinker/Tinker/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,8 +83,12 @@ WSGI_APPLICATION = "Tinker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'GYKakoLMAgpxIruzPOvubNrvjPnngBOh',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '59567',
     }
 }
 
@@ -127,3 +133,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
